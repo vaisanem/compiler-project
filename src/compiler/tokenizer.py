f8cf680683b7cc5cc24d9d1c1ae015151085a399
Miikka Väisänen <miikka.vaisanen@vincit.fi>
2024-02-16 11:13:05 +0200

    Operators, punctuation and comments

--- src/compiler/tokenizer.py
+++ src/compiler/tokenizer.py
@@ -6,48 +6,67 @@ from enum import Enum
 whitepace = re.compile(r"\s");
 identifier = re.compile("[a-zA-Z_][a-zA-Z0-9_]*")
 int_literal = re.compile("[0-9]+")
+operator = re.compile("[=!<>]=|[=<>%*/+-]")
+punctuation = re.compile("[(){},;]")
+comment = re.compile(r"(//|#).*")
 
 invalid_tokens = re.compile("[0-9]+[a-zA-Z_]+") #int_literal and identifier have to be separated with whitespace
 
 class Type(Enum):
-	IDENTIFIER = 1
-	INT_LITERAL = 2
+    IDENTIFIER = 1
+    INT_LITERAL = 2
+    OPERATOR = 3
+    PUNCTUATION = 4
 
 @dataclass	
 class Position:
-	line : int
-	column : int
+    line : int
+    column : int
 
 @dataclass
 class Token:
-	type: Type
-	content : str
-	position : Position = field(default_factory=lambda: Position(0,0))
+    type: Type
+    content : str
+    position : Position = field(default_factory=lambda: Position(0,0))
 
 def tokenize(source_code: str) -> list[Token]:
-	#TODO other token types, positioning
-	tokens = []
-	while source_code:
-		match = whitepace.match(source_code)
-		if (match):
-			source_code = source_code[match.end():]
-			continue
-		match = invalid_tokens.match(source_code)
-		if (match):
-			raise SyntaxError("Wrong syntax")
-			return []
-		match = identifier.match(source_code)
-		if (match):
-			token = match.group();
-			tokens.append(Token(Type.IDENTIFIER, token))
-			source_code = source_code[match.end():]
-			continue
-		match = int_literal.match(source_code)
-		if (match):
-			token = match.group();
-			tokens.append(Token(Type.INT_LITERAL, token))
-			source_code = source_code[match.end():]
-			continue
-		raise SyntaxError("Wrong syntax")
-		return []
-	return tokens
+    #TODO positioning
+    tokens = []
+    while source_code:
+        match = whitepace.match(source_code)
+        if (match):
+            source_code = source_code[match.end():]
+            continue
+        match = comment.match(source_code)
+        if (match):
+            source_code = source_code[match.end():]
+            continue
+        match = invalid_tokens.match(source_code)
+        if (match):
+            raise SyntaxError("Wrong syntax")
+        match = identifier.match(source_code)
+        if (match):
+            token = match.group();
+            tokens.append(Token(Type.IDENTIFIER, token))
+            source_code = source_code[match.end():]
+            continue
+        match = int_literal.match(source_code)
+        if (match):
+            token = match.group();
+            tokens.append(Token(Type.INT_LITERAL, token))
+            source_code = source_code[match.end():]
+            continue
+        match = operator.match(source_code)
+        if (match):
+            token = match.group();
+            tokens.append(Token(Type.OPERATOR, token))
+            source_code = source_code[match.end():]
+            continue
+        match = punctuation.match(source_code)
+        if (match):
+            token = match.group();
+            tokens.append(Token(Type.PUNCTUATION, token))
+            source_code = source_code[match.end():]
+            continue
+        raise SyntaxError("Wrong syntax")
+    return tokens
--- tests/tokenizer_test.py
+++ tests/tokenizer_test.py
@@ -2,35 +2,51 @@ import pytest
 from compiler.tokenizer import tokenize, Token, Type
 
 def test_tokenize_outputs_a_list() -> None:
-	assert type(tokenize("")) == list
-	
+    assert type(tokenize("")) == list
+    
+def test_tokenize_ignores_comments() -> None:
+    tokens = tokenize(" // then \n//\n# else \n #")
+    assert tokens == []
+    
 def test_identifiers_are_tokenized() -> None:
-	tokens = tokenize("if _666 var  or AND _a6   \nasd then else a_1_B ____")
-	assert tokens == [Token(Type.IDENTIFIER, "if"), Token(Type.IDENTIFIER, "_666"), Token(Type.IDENTIFIER, "var"), Token(Type.IDENTIFIER, "or"), 
-				   Token(Type.IDENTIFIER, "AND"), Token(Type.IDENTIFIER, "_a6"), Token(Type.IDENTIFIER, "asd"), Token(Type.IDENTIFIER, "then"), 
-				   Token(Type.IDENTIFIER, "else"), Token(Type.IDENTIFIER, "a_1_B"), Token(Type.IDENTIFIER, "____")]
-	
+    tokens = tokenize("if _666 var  or AND _a6   \nasd then else a_1_B not ____")
+    assert tokens == [Token(Type.IDENTIFIER, "if"), Token(Type.IDENTIFIER, "_666"), Token(Type.IDENTIFIER, "var"), Token(Type.IDENTIFIER, "or"), 
+                   Token(Type.IDENTIFIER, "AND"), Token(Type.IDENTIFIER, "_a6"), Token(Type.IDENTIFIER, "asd"), Token(Type.IDENTIFIER, "then"), 
+                   Token(Type.IDENTIFIER, "else"), Token(Type.IDENTIFIER, "a_1_B"), Token(Type.IDENTIFIER, "not"), Token(Type.IDENTIFIER, "____")]
+    
 def test_integer_literals_are_tokenized() -> None:
-	tokens = tokenize("666\n00000  1 1234567890")
-	assert tokens == [Token(Type.INT_LITERAL, "666"), Token(Type.INT_LITERAL, "00000"), Token(Type.INT_LITERAL, "1"), Token(Type.INT_LITERAL, "1234567890")]
-	
-def test_identifiers_and_integer_literals_are_tokenized() -> None:
-	tokens = tokenize("1 AND _a6   666\nasd a_1_B ____ 00000")
-	assert tokens == [Token(Type.INT_LITERAL, "1"), Token(Type.IDENTIFIER, "AND"), Token(Type.IDENTIFIER, "_a6"), Token(Type.INT_LITERAL, "666"), 
-				   Token(Type.IDENTIFIER, "asd"), Token(Type.IDENTIFIER, "a_1_B"), Token(Type.IDENTIFIER, "____"), Token(Type.INT_LITERAL, "00000")]
-	
+    tokens = tokenize("666\n00000  1 1234567890")
+    assert tokens == [Token(Type.INT_LITERAL, "666"), Token(Type.INT_LITERAL, "00000"), Token(Type.INT_LITERAL, "1"), Token(Type.INT_LITERAL, "1234567890")]
+    
+def test_operators_are_tokenized() -> None:
+    tokens = tokenize("==!= < +<=> >=/ % *- ")
+    assert tokens == [Token(Type.OPERATOR, "=="), Token(Type.OPERATOR, "!="), Token(Type.OPERATOR, "<"), Token(Type.OPERATOR, "+"), 
+                      Token(Type.OPERATOR, "<="), Token(Type.OPERATOR, ">"), Token(Type.OPERATOR, ">="), Token(Type.OPERATOR, "/"),
+                      Token(Type.OPERATOR, "%"), Token(Type.OPERATOR, "*"), Token(Type.OPERATOR, "-")]
+    
+def test_punctuation_marks_are_tokenized() -> None:
+    tokens = tokenize("(;}){ ,")
+    assert tokens == [Token(Type.PUNCTUATION, "("), Token(Type.PUNCTUATION, ";"), Token(Type.PUNCTUATION, "}"), Token(Type.PUNCTUATION, ")"),
+                      Token(Type.PUNCTUATION, "{"), Token(Type.PUNCTUATION, ",")]
+    
+def test_all_token_types_are_tokenized() -> None:
+    tokens = tokenize("//hello world\n 1) AND==666 {\na_1_B -__ 00 +#x= _4")
+    assert tokens == [Token(Type.INT_LITERAL, "1"), Token(Type.PUNCTUATION, ")"), Token(Type.IDENTIFIER, "AND"), Token(Type.OPERATOR, "=="),
+                      Token(Type.INT_LITERAL, "666"), Token(Type.PUNCTUATION, "{"), Token(Type.IDENTIFIER, "a_1_B"), Token(Type.OPERATOR, "-"),
+                      Token(Type.IDENTIFIER, "__"), Token(Type.INT_LITERAL, "00"), Token(Type.OPERATOR, "+")]
+    
 def test_tokenize_raises_error_for_unsupported_characters() -> None:
-	with pytest.raises(SyntaxError, match="Wrong syntax"):
-		tokenize("if p == 0")
-	with pytest.raises(SyntaxError, match="Wrong syntax"):
-		tokenize("&")
-	with pytest.raises(SyntaxError, match="Wrong syntax"):
-		tokenize("ä")
-		
-def test_tokenize_raises_error_for_invalid_token() -> None:
-	with pytest.raises(SyntaxError, match="Wrong syntax"):
-		tokenize("23 else 6a")
-	
-def test_tokens_that_are_not_separated_by_whitespace_are_tokenized() -> None:
-	#operators or parenthesis etc
-	assert 0 == 1
+    with pytest.raises(SyntaxError, match="Wrong syntax"):
+        tokenize("var x = (A && b) ? TRUE : FALSE")
+    with pytest.raises(SyntaxError, match="Wrong syntax"):
+        tokenize("&")
+    with pytest.raises(SyntaxError, match="Wrong syntax"):
+        tokenize("ä")
+        
+def test_tokenize_raises_error_for_invalid_tokens() -> None:
+    with pytest.raises(SyntaxError, match="Wrong syntax"):
+        tokenize("23 else 6a")
+    with pytest.raises(SyntaxError, match="Wrong syntax"):
+        tokenize("23 else 6A")
+    with pytest.raises(SyntaxError, match="Wrong syntax"):
+        tokenize("23 else 6_")
