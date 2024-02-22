91d3bdbee0cbec6f00d70451ff37fd94e013ae32
Miikka Väisänen <miikka.vaisanen@vincit.fi>
2024-02-22 22:14:56 +0200

    More parsing

--- src/compiler/parser.py
+++ src/compiler/parser.py
@@ -2,13 +2,16 @@
 import compiler.ast as ast
 from compiler.tokenizer import Token, Type
 
-def parse(tokens: list[Token]) -> ast.Expression: #rename pos to index?
+def parse(tokens: list[Token]) -> ast.Expression: #rename pos to index etc?
     #TODO: refactor and add tests
     pos = 0
     
     def peek() -> Token:
         if not pos < len(tokens):
-            return Token(Type.END, "", tokens[-1].position) #Type = null?
+            if tokens:
+                return Token(Type.END, "", tokens[-1].position)
+            else:
+                return Token(Type.END, "")
         return tokens[pos]
     
     # 'consume(expected)' returns the token at 'pos'
@@ -34,23 +37,36 @@ def parse(tokens: list[Token]) -> ast.Expression: #rename pos to index?
             raise Exception(f'{peek().position}: expected integer literal')
         return ast.Literal(int(consume().content))
     
+    def parse_bool_literal() -> ast.Expression:
+        if not peek().type == Type.BOOL_LITERAL:
+            raise Exception(f'{peek().position}: expected boolean literal')
+        return ast.Literal(consume().content == "true")
+    
     def parse_identifier() -> ast.Expression:
         if not peek().type == Type.IDENTIFIER:
             raise Exception(f'{peek().position}: expected identifier')
         return ast.Identifier(consume().content)
     
+    def parse_parentheses() -> ast.Expression:
+        consume("(")
+        exp = parse_expression()
+        consume(")")
+        return exp
+    
     def parse_factor() -> ast.Expression:
+        if peek().content == '(':
+            return parse_parentheses()
         if peek().type == Type.INT_LITERAL:
             return parse_int_literal()
         if peek().type == Type.IDENTIFIER:
             return parse_identifier()
-        raise Exception(f'{peek().position}: expected your mum')
+        raise Exception(f'{peek().position}: expected "(", integer literal or identifier')
     
     def parse_term() -> ast.Expression:
         exp = parse_factor()
         while peek().type == Type.OPERATOR:
-            if peek().content in ["*", "/"]:
-                op = consume(["*", "/"]).content
+            if peek().content in ['*', '/']:
+                op = consume().content
                 right = parse_factor()
                 exp = ast.BinaryOp(exp, op, right)
         return exp
@@ -58,10 +74,12 @@ def parse(tokens: list[Token]) -> ast.Expression: #rename pos to index?
     def parse_expression() -> ast.Expression:
         exp = parse_term()
         while peek().type == Type.OPERATOR:
-            if peek().content in ["+", "-"]:
-                op = consume(["+", "-"]).content
+            if peek().content in ['+', '-']:
+                op = consume().content
                 right = parse_term()
                 exp = ast.BinaryOp(exp, op, right)
+        if peek().type != Type.END: 
+            raise Exception(f'{peek().position}: unexpected token')
         return exp
     
     return parse_expression()
--- src/compiler/tokenizer.py
+++ src/compiler/tokenizer.py
@@ -3,21 +3,25 @@ from dataclasses import dataclass, field
 import re
 from enum import Enum
 
-whitepace = re.compile(r"\s");
-identifier = re.compile("[a-zA-Z_][a-zA-Z0-9_]*")
+whitepace = re.compile(r'\s');
 int_literal = re.compile("[0-9]+")
-operator = re.compile("[=!<>]=|[=<>%*/+-]")
+bool_literal = re.compile("true|false")
+keyword = re.compile("if|then|else|while|do|return|var") #unit, reserved identifiers?
+identifier = re.compile("[a-zA-Z_][a-zA-Z0-9_]*")
+operator = re.compile("[=!<>]=|[=<>%*/+-]") #and, or, not? Tokenize before identifiers
 punctuation = re.compile("[(){},;]")
-comment = re.compile(r"(//|#).*")
+comment = re.compile(r'(//|#).*')
 
-invalid_tokens = re.compile("[0-9]+[a-zA-Z_]+") #int_literal and identifier have to be separated with whitespace
+invalid_tokens = re.compile("[0-9]+[a-zA-Z_]+") #integer literal and boolean literal/identifier have to be separated with whitespace
 
 class Type(Enum):
-    IDENTIFIER = 1
-    INT_LITERAL = 2
-    OPERATOR = 3
-    PUNCTUATION = 4
-    END = 5
+    INT_LITERAL = 1
+    BOOL_LITERAL = 2
+    KEYWORD = 3
+    IDENTIFIER = 3
+    OPERATOR = 4
+    PUNCTUATION = 5
+    END = 6
 
 @dataclass	
 class Position:
@@ -31,7 +35,7 @@ class Token:
     position : Position = field(default_factory=lambda: Position(0,0))
 
 def tokenize(source_code: str) -> list[Token]:
-    #TODO positioning
+    #TODO: positioning
     tokens = []
     while source_code:
         match = whitepace.match(source_code)
@@ -45,16 +49,28 @@ def tokenize(source_code: str) -> list[Token]:
         match = invalid_tokens.match(source_code)
         if (match):
             raise SyntaxError("Wrong syntax")
-        match = identifier.match(source_code)
-        if (match):
-            token = match.group();
-            tokens.append(Token(Type.IDENTIFIER, token))
-            source_code = source_code[match.end():]
-            continue
         match = int_literal.match(source_code)
         if (match):
             token = match.group();
             tokens.append(Token(Type.INT_LITERAL, token))
+            source_code = source_code[match.end():]
+            continue
+        match = bool_literal.match(source_code)
+        if (match): #TODO: add tests
+            token = match.group();
+            tokens.append(Token(Type.BOOL_LITERAL, token))
+            source_code = source_code[match.end():]
+            continue
+        match = keyword.match(source_code)
+        if (match): #TODO: add tests
+            token = match.group();
+            tokens.append(Token(Type.KEYWORD, token))
+            source_code = source_code[match.end():]
+            continue
+        match = identifier.match(source_code)
+        if (match):
+            token = match.group();
+            tokens.append(Token(Type.IDENTIFIER, token))
             source_code = source_code[match.end():]
             continue
         match = operator.match(source_code)
